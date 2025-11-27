4d2807abd3505fe9f29f49cbe266a2120d6b6806
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-11-27 19:04:59 +0500

    Add project files.

---
+++ Tyuiu.ReshetovaAA.Sprint4.Task0.V16.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ReshetovaAA.Sprint4.Task0.V16.Lib
+{
+    public class DataService : ISprint4Task0V16
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int p = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    p *= array[i];
+                }
+            }
+
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.ReshetovaAA.Sprint4.Task0.V16/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.ReshetovaAA.Sprint4.Task0.V16.Lib;
+
+namespace Tyuiu.ReshetovaAA.Sprint4.Task0.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Решетова А. А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Решетова Арина Алексеевна | СМАРТб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан массив из 10 элементов (0–9). Подсчитать произведение чётных        *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+
+            Console.WriteLine("Массив: 2, 6, 2, 3, 4, 5, 4, 9, 7, 8");
+
+            int result = ds.GetMultEvenArrEl(array);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Произведение чётных элементов = {result}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Программа завершена. Нажмите любую клавишу для выхода.                 *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.ReadKey();
+        }
+    }
+}
